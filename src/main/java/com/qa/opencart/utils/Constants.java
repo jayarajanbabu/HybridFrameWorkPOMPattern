20f670ced9f9f751bfabf511b039ed1969f34d31
“jaya” <jayasubburayaluj@gmail.com>
2021-12-17 20:40:01 -0500

    updated TimeOut value

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 	public static final String LOGIN_PAGE_TITLE= "Account Login";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
-	public static final int DEFAULT_TIME_OUT = 5;
+	public static final int DEFAULT_TIME_OUT = 6;
 	public static final String ACCOUNTS_PAGE_HEADER = "Your Store";
 	
 	
